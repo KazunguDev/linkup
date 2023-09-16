0d730320d5c865f166a8f1a3720de5f4df0d83ad
KazunguDev <kazungudev@gmail.com>
2023-09-16 21:54:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# linkup
+
+A Full Contacts app build using flutter and firebase.
+
+## Screenshoots
+
---
+++ android/app/src/main/kotlin/com/kazungudev/linkup/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kazungudev.linkup
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
